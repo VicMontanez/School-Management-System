06ecfd2c5cf32a59f94987076e2c16cf26e82d68
VicMontanez <v_montanez@hotmail.com>
2020-04-06 22:57:06 -0400

    added fields to teacher class

---
+++ src/school/management/system/Teacher.java
@@ -0,0 +1,11 @@
+package school.management.system;
+
+/**
+ * This class is responsible for keeping track of teachers name, id ,salary
+ */
+public class Teacher {
+
+    private int id;
+    private String name;
+    private int salary;
+}
