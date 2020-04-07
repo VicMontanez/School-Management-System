01a22c3f47a904ca2c72c21f45db0d262d2b0740
VicMontanez <v_montanez@hotmail.com>
2020-04-06 22:37:52 -0400

    created setter for grade

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -15,14 +15,25 @@ public class Student {
 
     /*
     * To create a new student by initializing
+    * Fees for every student is $30,000
+    * Fees paid initially is 0.
     * */
 
     public Student(int id, String name, int grade) {
-
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
         this.id = id;
         this.name = name;
         this.grade = grade;
     }
+
+    //Not going to alter student's name, student's id.
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+
 
 
 }
