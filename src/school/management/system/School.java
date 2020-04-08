ff3204bfad9b87e6a6b2de018144fda48e9c53f3
VicMontanez <v_montanez@hotmail.com>
2020-04-07 23:01:01 -0400

    added array list to student  value

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class School {
 
     private List<Teacher> teacher;
-    private Student student;
+    private List<Student> student;
     private int totalMoneyEarned;
     private int totalMoneySpent;
 }
