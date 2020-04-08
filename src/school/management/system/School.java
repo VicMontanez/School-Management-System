414408d29838d8848dcb1ea81b436a9df1899757
VicMontanez <v_montanez@hotmail.com>
2020-04-07 23:03:35 -0400

    created constructor for school

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -8,4 +8,14 @@ public class School {
     private List<Student> student;
     private int totalMoneyEarned;
     private int totalMoneySpent;
+
+    /**
+     * new school object is created
+     * @param teacher list of teachers in the school
+     * @param student list of students in the school
+     */
+    public School(List<Teacher> teacher, List<Student> student) {
+        this.teacher = teacher;
+        this.student = student;
+    }
 }
