96e180512178e0edf6795d1ba24cafd7d5fc6bf4
VicMontanez <v_montanez@hotmail.com>
2020-04-10 21:32:16 -0400

    created teacher list

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -1,5 +1,8 @@
 package school.management.system;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +10,15 @@ public class Main {
         Teacher melissa = new Teacher(2, "Melissa", 700);
         Teacher vanderhorn = new Teacher(3, "Vanderhorn", 600);
 
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(lizzy);
+        teacherList.add(melissa);
+        teacherList.add(vanderhorn);
+
         Student tanasha = new Student(1, "Tamasha", 4);
         Student rakshith = new Student(2, "Rakshith V", 12);
         Student rabbi = new Student(3, "Rabbi", 5);
+
+        School ghs = new School()
     }
 }
