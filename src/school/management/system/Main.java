a1e0fd4b0b545e385302fba8dae7ca24abc28a00
VicMontanez <v_montanez@hotmail.com>
2020-04-10 21:25:30 -0400

    created new teacher

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    Teacher lizzy = new Teacher(1, "Lizzy", 500);
         Teacher melissa = new Teacher(2, "Melissa", 700);
-
+        Teacher vanderhorn = new Teacher(3, "Vanderhorn", 600);
     }
 }
