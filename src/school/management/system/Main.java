918b2850c1b198b648c901b67e08b3f4946f16af
VicMontanez <v_montanez@hotmail.com>
2020-04-10 22:33:12 -0400

    used addTeacher method to add new teacher

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -15,15 +15,34 @@ public class Main {
         teacherList.add(melissa);
         teacherList.add(vanderhorn);
 
-        Student tanasha = new Student(1, "Tamasha", 4);
+        Student tamasha = new Student(1, "Tamasha", 4);
         Student rakshith = new Student(2, "Rakshith V", 12);
         Student rabbi = new Student(3, "Rabbi", 5);
 
         List<Student> studentList = new ArrayList<>();
-        studentList.add(tanasha);
+        studentList.add(tamasha);
         studentList.add(rakshith);
         studentList.add(rabbi);
 
-        School ghs = new School()
+        School ghs = new School(teacherList, studentList);
+
+        Teacher megan = new Teacher(6, "Megan", 900);
+
+        ghs.addTeacher(megan);
+
+
+        tamasha.payFees(5000);
+        rakshith.payFees(6000);
+        System.out.println("GHS has earned $" + ghs.getTotalMoneyEarned());
+
+        System.out.println("-----Making SCHOOL PAY SALARY-----");
+        lizzy.receiveSalary(lizzy.getSalary());
+        System.out.println("GHS has spent for salary to " + lizzy.getName() + " and now has $" + ghs.getTotalMoneyEarned());
+
+        vanderhorn.receiveSalary(vanderhorn.getSalary());
+        System.out.println("GHS has spent for salary to " + vanderhorn.getName() + " and now has $" + ghs.getTotalMoneyEarned());
+
+        System.out.println(rakshith);
+        System.out.println(melissa);
     }
 }
