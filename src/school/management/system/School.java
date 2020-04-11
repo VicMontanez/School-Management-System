fe6c41cc5fb2cf9f1e19f390c007c189c38ba33f
VicMontanez <v_montanez@hotmail.com>
2020-04-10 21:20:15 -0400

    update total money spent method

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -12,9 +12,9 @@ public class School {
     /**
      * new school object is created
      * @param teachers list of teachers in the school
-     * @param student list of students in the school
+     * @param students list of students in the school
      */
-    public School(List<Teacher> teachers, List<Student> student) {
+    public School(List<Teacher> teachers, List<Student> students) {
         this.teachers = teachers;
         this.students = students;
         totalMoneyEarned = 0;
@@ -69,11 +69,20 @@ public class School {
         totalMoneyEarned += totalMoneyEarned;
     }
 
+    /**
+     *
+     * @return the total money spent by the school
+     */
     public int getTotalMoneySpent() {
         return totalMoneySpent;
     }
 
-    public void setTotalMoneySpent(int totalMoneySpent) {
-        this.totalMoneySpent = totalMoneySpent;
+    /**
+     * update the money that is spent by the school which is the salary given by the school to its teachers.
+     *
+     * @param moneySpent money spent by school
+     */
+    public void updateTotalMoneySpent(int moneySpent) {
+            totalMoneyEarned-=moneySpent;
     }
 }
